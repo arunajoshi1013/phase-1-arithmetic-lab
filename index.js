f9e70d4d58644da83c2a64c051b727d8389e3818
Aruna Joshi <arunajoshi1013@gmail.com>
2023-03-14 14:20:40 -0700

    Arthimetic expression

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
-// Write your code here
+const num1=16;
+const num2=4;
+let multiply=num1*num2-2;
+console.log(multiply);
+let random=100;
+console.log(Number.isInteger(random));
+let num3=68;
+let num4=8;
+let mod=num3 %num4;
+console.log(mod);
+let max=Math.max(10, 20)
+console.log(max);
